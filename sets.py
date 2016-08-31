f9c7eef2f1d3114c6de41385c39cc9dd901c4687
MomchilAngelov <momchil.angelov.1997@gmail.com>
2016-08-31 15:15:33 +0300

    Draft for set and different conditions

--- sets.py
+++ sets.py
@@ -1,22 +1,128 @@
-ivan = ['пушене', 'пиене', 'тия три неща', 'коли', 'facebook', 'игри', 'разходки по плажа', 'скандинавска поезия']
-maria = ['пиене', 'мода', 'facebook', 'игри', 'лов със соколи', 'шопинг', 'кино']
+#ivan = ['пушене', 'пиене', 'тия три неща', 'коли', 'facebook', 'игри', 'разходки по плажа', 'скандинавска поезия']
+#maria = ['пиене', 'мода', 'facebook', 'игри', 'лов със соколи', 'шопинг', 'кино']
+#for val in [set(ivan) & set(maria)]:
+#	print(val)
 
-for val in [set(ivan) & set(maria)]:
-	print(val)
+people = [
+    {
+        'name': "Мария",
+        'interests': ['пътуване', 'танци', 'плуване', 'кино'],
+        'age': 24,
+        'gender': "female",
+        "ex": ["Кирил", "Петър"],
+    },
+    {
+        'name': "Диана",
+        'interests': ['мода', 'спортна стрелба', 'четене', 'скандинавска поезия'],
+        'age': 21,
+        'gender': "female",
+        "ex": [],
+    },
+    {
+        'name': "Дарина",
+        'interests': ['танци', 'покер', 'история', 'софтуер'],
+        'age': 34,
+        'gender': "female",
+        "ex": ["Борис"],
+    },
+    {
+        'name': "Лилия",
+        'interests': ['покер', 'автомобили', 'танци', 'кино'],
+        'age': 36,
+        'gender': "female",
+        "ex": [],
+    },
+    {
+        'name': "Галя",
+        'interests': ['пътуване', 'автомобили', 'плуване', 'баскетбол'],
+        'age': 18,
+        'gender': "female",
+        "ex": ['Димитър'],
+    },
+    {
+        'name': "Валерия",
+        'interests': ['плуване', 'покер', 'наука', 'скандинавска поезия'],
+        'age': 27,
+        'gender': "female",
+        "ex": [],
+    },
+    {
+        'name': "Ина",
+        'interests': ['кино', 'лов със соколи', 'пътуване', 'мода'],
+        'age': 20,
+        'gender': "female",
+        "ex": [],
+    },
+    {
+        'name': "Кирил",
+        'interests': ['баскетбол', 'автомобили', 'кино', 'наука'],
+        'age': 19,
+        'gender': "male",
+        'ex': ["Мария"],
+    },
+    {
+        'name': "Георги",
+        'interests': ['автомобили', 'футбол', 'плуване', 'танци'],
+        'age': 32,
+        'gender': "male",
+        'ex': [],
+    },
+    {
+        'name': "Андрей",
+        'interests': ['футбол', 'скандинавска поезия', 'история', 'танци'],
+        'age': 26,
+        'gender': "male",
+        'ex': ["Мария"],
+    },
+    {
+        'name': "Емил",
+        'interests': ['летене', 'баскетбол', 'софтуер', 'наука'],
+        'age': 34,
+        'gender': "male",
+        'ex': ['Дарина'],
+    },
+    {
+        'name': "Димитър",
+        'interests': ['футбол', 'лов със соколи', 'автомобили', 'баскетбол'],
+        'age': 22,
+        'gender': "male",
+        'ex': ['Галя'],
+    },
+    {
+        'name': "Петър",
+        'interests': ['пътуване', 'покер', 'баскетбол', 'лов със соколи'],
+        'age': 23,
+        'gender': "male",
+        'ex': ["Мария"],
+    },
+    {
+        'name': "Калоян",
+        'interests': ['кино', 'покер', 'пътуване', 'автомобили'],
+        'age': 29,
+        'gender': "male",
+        'ex': [],
+    },
+]
 
+for human in people:
+	gender = human.get('gender')
+	name = human.get('name')
+	interests = set(human.get('interests'))
+	years = human.get('age')
+	exes = set(human.get('ex'))
 
-
-
-
-
-
-
-
-
-'''
-	_list = ["test1", "test2", "test3", "test4"]
-	print(_list[2])
-	del _list[2] 
-	for inx, item in enumerate(_list):
-		print("index: " + str(inx) + "\nItem: " + item)
-'''
+	for other_humans in people:
+		gender_other = other_humans.get('gender')
+		if not gender_other is gender:
+			interests_other = set(other_humans.get('interests'))
+			years_other = other_humans.get('age')
+			mutual_interests = interests & interests_other
+			age_difference = abs(years - years_other)
+			exes_other = set(other_humans.get('ex'))
+			mutual_exes = exes & exes_other
+			if (len(mutual_interests) > 0) and (age_difference < 7) and (len(mutual_exes) == 0):
+				name_other = other_humans.get('name')
+				print("Влюбихме " + name + " и " + name_other + " на база това, че имат " + str(age_difference) + " години разлика, и че те харесват ", end="")
+				for key in mutual_interests:
+					print(key, end=", ")
+				print("")
