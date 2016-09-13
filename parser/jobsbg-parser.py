a508d31e3c89b79d10ff49c658ab729999c3a294
MomchilAngelov <momchil.angelov.1997@gmail.com>
2016-09-13 16:37:18 +0300

    Re-writing the jobs/python parser to use BeatifulSoup - and it's a lot better and more concise than my other "code"

--- parser/jobsbg-parser.py
+++ parser/jobsbg-parser.py
@@ -2,12 +2,12 @@ import urllib3
 import time
 import sys
 import re
-
-from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 
 urllib3.disable_warnings()
 
 number_of_results = 0
+max_page = []
 city = 'sofia'
 city_data = {
 	'sofia': 1,
@@ -28,145 +28,47 @@ base_string = "https://www.jobs.bg/front_job_search.php?frompage={0}&is_region=&
 python_string = "https://www.python.org/jobs/?page={0}"
 
 regex_results = re.compile(r'[0-9] - [0-9][0-9] от (\d+)')
+python_page_regex = re.compile(r'\?page=(\d+)')
 python_net_regex = re.compile(r"/jobs/\d+/")
-control_regex = re.compile(r"doesnt_matter")
-always_regex = re.compile(r"[\d\w\W]*")
-
-def is_regex(regex):
-	return type(control_regex) == type(regex) 
-
-
-class ResultsCountGetter(HTMLParser):
-	def __init__(self):
-		HTMLParser.__init__(self)
-		self.recording = 0 
-		self.data = []
-	def handle_starttag(self, tag, attrs):
-		if tag == "td":
-			self.recording = 1
-
-	def handle_endtag(self, tag):
-		if tag == 'td':
-			self.recording -= 1
-
-	def handle_data(self, data):
-		if self.recording:
-			m = regex_results.match(data)
-			if m:
-				global number_of_results
-				number_of_results = int(m.groups()[0])
-
-
-#https://www.python.org/jobs/
-class PagesGetter(HTMLParser):
-	def __init__(self):
-		HTMLParser.__init__(self)
-		self.recording = 0 
-		self.data = []
-	def handle_starttag(self, tag, attrs):
-		if tag == "a" and "?page=" in attrs[0]:
-			self.recording = 1
-		
-	def handle_endtag(self, tag):
-		if tag == 'a':
-			self.recording -= 1
-		
-	def handle_data(self, data):
-		if self.recording:
-			try:
-				value = int(data)
-				self.data.append(value)
-			except Exception as e:
-				pass
-
-
-class PythonJobsParser(HTMLParser):
-	def __init__(self, file_handler, link_appender, query_string, search_arguments, http):
-		HTMLParser.__init__(self)
-		self.recording = 0 
-		self.data = []
-		self.link_string = ""
-		self.file_handler = file_handler
-		self.link_appender = link_appender
-		self.query_string = query_string
-		self.search_arguments = search_arguments
-		self.http = http
-		self.flag = 0
-
-	def check_tag(self, tag):
-		return tag in self.search_arguments
-
-	def check_attribute(self, data, search_for):
-		if is_regex(search_for):
-			if search_for.search(data):
-				return True
-		else:
-			if search_for == data:
-				return True
-		return False
-
-	def handle_starttag(self, tag, attrs):
-		for search_argument in self.search_arguments:
-			if self.check_tag(tag):
-				lists_with_requirements = self.search_arguments.get(tag)
-				for i in range(len(lists_with_requirements)):
-					for name, value in attrs:
-						if (self.check_attribute(data = name, search_for = lists_with_requirements[i][0])
-								and self.check_attribute(data = value, search_for = lists_with_requirements[i][1])):
-							self.flag += 1
-							if self.flag == len(lists_with_requirements):
-								self.link_string = value
-								self.recording = 1
-
-	def handle_endtag(self, tag):
-		for search_argument in search_arguments:
-			if tag == search_argument:
-				self.recording = 0
-				self.flag = 0
-
-	def handle_data(self, data):
-		if self.recording:
-			self.data.append(data + self.link_appender + self.link_string)
-
-	def work(self, iteration_argument):
-		request = http.request('GET', self.query_string.format(iteration_argument))
-		self.feed(str(request.data, 'utf-8'))
-		for line in self.data:
-			self.file_handler.write("{0}\n".format(line.strip()))
-		self.data = []
-
-counter = ResultsCountGetter()
 
 http = urllib3.PoolManager()
 
-request = http.request('GET', base_string.format(0))
-counter.feed(str(request.data, 'utf-8'))
-counter.close()
-
-print("Number of results:", number_of_results)
-
 with open("jobsbgparserdata", "w") as f:
-	search_arguments = { 'a':[["class", "joblink"], ["href", always_regex], ], }
-	jobs_com_parser = PythonJobsParser(file_handler = f, search_arguments = search_arguments, http = http,
-										link_appender =  " | https://www.jobs.bg/", query_string = base_string)
 
+	request = http.request('GET', base_string.format(0))
+	soup = BeautifulSoup(str(request.data, 'utf-8'), 'html.parser')
+	for data in soup.find_all('td'):
+		groups = regex_results.match(str(data.string))
+		if groups:
+			number_of_results = (int(groups.groups()[0]))
+			break
+
+	print("Number of results:", number_of_results)
 	for x in range(0, number_of_results, 15):
-		print("Making query on result:",x,"-",x+15)
-		jobs_com_parser.work(iteration_argument = x)
+		print("Query {0}-{1}".format(x, x+15))
+		request = http.request('GET', base_string.format(x))
+		soup = BeautifulSoup(str(request.data, 'utf-8'), 'html.parser')
+		for data in soup.find_all('a'):
+			if data.get('class'):
+				if data.get('class')[0] == "joblink":
+					f.write("{0} | {1}\n".format(data.string, "https://www.jobs.bg/"+data.get('href')))
 
 	f.write("SENTINEL DATA\n")
-	
-	page_count_getter = PagesGetter()
-	search_arguments = { 'a':[["href", python_net_regex], ], }
-	python_org_parser = PythonJobsParser(file_handler = f, search_arguments = search_arguments, http = http,
-										link_appender =  " | https://www.python.org", query_string = python_string)
-	
+
 	request = http.request('GET', python_string.format(1))
+	soup = BeautifulSoup(str(request.data, 'utf-8'), 'html.parser')
+	for link_href in soup.find_all('a'):
+		data = link_href.get('href')
+		if '?page=' in data:
+			m = python_page_regex.match(data)
+			max_page.append(int(m.groups()[0]))
 
-	page_count_getter.feed(str(request.data, 'utf-8'))
-	page_count_getter.close()
-
-	pages = max(page_count_getter.data)
-
-	for x in range(1, pages + 1):
-		python_org_parser.work(iteration_argument = x)
+	max_page = max(max_page)
+	for x in range(1, max_page+1):
+		print("Page {0}".format(x))
+		request = http.request('GET', python_string.format(x))
+		soup = BeautifulSoup(str(request.data, 'utf-8'), 'html.parser')
+		for data in soup.find_all('a'):
+			m = python_net_regex.match(data.get('href'))
+			if m:
+				f.write("{0} | {1}\n".format(data.string, "https://www.python.org" + data.get('href')))
